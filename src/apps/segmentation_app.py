971b8eb596ae7f678b9225bfbb58f9232fb57de1
qhuy0801 <qhuy0168@gmail.com>
2023-08-03 17:16:07 +0100

    :memo: Remove redundant methods in the app file

--- src/apps/segmentation_app.py
+++ src/apps/segmentation_app.py
@@ -5,55 +5,16 @@ import torch
 
 import constraints.SEG_CONST as CONSTRAINTS
 from entities.Image import Image
-from utils.data_utils import init_images
-
-
-def transform_image(_target_height: int, _target_width: int):
-    """
-    Image transformation template
-    :param _target_height:
-    :param _target_width:
-    :return:
-    """
-    return A.Compose(
-        [
-            A.Resize(_target_height, _target_width, interpolation=cv2.INTER_NEAREST),
-            A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    )
-
-
-def initialise_image_matrix(_image_instance: Image, _transform):
-    """
-
-    :param _image_instance:
-    :param _transform:
-    :return:
-    """
-    _image_instance.load_image()
-    image_matrix = _image_instance.img
-    return _transform(image=image_matrix)["image"]
-
-
-def get_segmentation_matrix(_model, _matrix):
-    """
-
-    :param _model:
-    :param _matrix:
-    :return:
-    """
-    return np.argmax((_model(torch.Tensor(_matrix))).detach().numpy(), axis=1)
+from utils.data_utils import init_images, get_metadata
 
 
 if __name__ == "__main__":
-    # Get the images
-    images = init_images(
+
+    # Get the metadata
+    metadata = get_metadata(
         CONSTRAINTS.METADATA_PATH,
-        CONSTRAINTS.IMAGES_DIR,
         CONSTRAINTS.SAMPLE_COUNT,
-        CONSTRAINTS.FILENAME_COLUMN,
+        ("ImageDetail.hasRuler", None, True)
     )
-    for image in images:
-        image.load_image()
 
     print("Testing...")
