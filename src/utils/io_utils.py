ff2b046af5400ad4191edbf3cc4d706eb0af962b
qhuy0801 <qhuy0168@gmail.com>
2023-08-14 11:05:50 +0100

    :package: Diffuser.py EMA.py

--- src/utils/io_utils.py
+++ src/utils/io_utils.py
@@ -8,7 +8,14 @@ def find_path(root_dir, filename):
     :param filename:
     :return:
     """
-    for root, dirs, files in os.walk(root_dir):
+    for root, _, files in os.walk(root_dir):
         if filename in files:
             return os.path.join(root, filename)
     return None
+
+
+def mk_folders(run_name):
+    os.makedirs("models", exist_ok=True)
+    os.makedirs("results", exist_ok=True)
+    os.makedirs(os.path.join("models", run_name), exist_ok=True)
+    os.makedirs(os.path.join("results", run_name), exist_ok=True)
