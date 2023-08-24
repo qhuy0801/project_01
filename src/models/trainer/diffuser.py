ca3ca3f31fc20d8b9e1b2e068dddf66bd2801d8a
qhuy0801 <qhuy0168@gmail.com>
2023-08-24 12:43:02 +0100

    :construction: diffuser.py constructor

--- src/models/trainer/diffuser.py
+++ src/models/trainer/diffuser.py
@@ -38,9 +38,24 @@ class Diffuser:
 
     # Training settings
     max_lr: float
+    eps: float
+    epochs: int
 
-    def __init__(self, max_lr: float = 1e-4) -> None:
+    def __init__(
+        self,
+        train_data: DataLoader,
+        embedder: Embedder,
+        beta_start: float = 1e-4,
+        beta_end: float = 0.02,
+        noise_steps: int = 1000,
+        max_lr: float = 1e-4,
+        eps: float = 1e-5,
+        epochs: int = 1000,
+        **kwargs
+    ) -> None:
         super().__init__()
+        self.train_data = train_data
+        self.embedder = embedder
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = UNet(
             in_channels=self.in_channels,
@@ -50,5 +65,22 @@ class Diffuser:
         self.loss_func = torch.nn.MSELoss()
         self.scaler = torch.cuda.amp.GradScaler()
 
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.noise_steps = noise_steps
+
         self.max_lr = max_lr
-        self.optimiser = torch.optim.AdamW(self.model.parameters(), lr=self.max_lr, )
+        self.eps = eps
+        self.epochs = epochs
+
+        self.optimiser = torch.optim.AdamW(
+            params=self.model.parameters(), lr=self.max_lr, eps=self.eps
+        )
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=self.optimiser,
+            max_lr=self.max_lr,
+            steps_per_epoch=len(self.train_data),
+            epochs=self.epochs,
+        )
+
+        self.__dict__.update(kwargs)
