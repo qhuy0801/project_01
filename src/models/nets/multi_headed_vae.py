f1a5563b1e726c0e4a19777aee1e1167a36fb057
qhuy0801 <qhuy0168@gmail.com>
2023-09-11 01:11:50 +0100

    :sparkles: Start constructing multi_headed_vae.py

--- src/models/nets/multi_headed_vae.py
+++ src/models/nets/multi_headed_vae.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+import torch
+
 from models.nets.vae import VAE
 
 
@@ -9,3 +11,29 @@ class MultHeadedVAE(VAE):
         # Make 2 more copy of the decoder
         self.decoder_1 = deepcopy(self.decoder)
         self.decoder_2 = deepcopy(self.decoder)
+
+    def decode_1(self, x):
+        """
+        Decode from latent space into pixel space
+        :param x:
+        :return:
+        """
+        x = self.decoder_input(x)
+        x = x.view(
+            -1, self.dims[-1][-1], self.compressed_conv_size, self.compressed_conv_size
+        )
+        x = self.decoder_1(x)
+        return torch.sigmoid(x)
+
+    def decode_2(self, x):
+        """
+        Decode from latent space into pixel space
+        :param x:
+        :return:
+        """
+        x = self.decoder_input(x)
+        x = x.view(
+            -1, self.dims[-1][-1], self.compressed_conv_size, self.compressed_conv_size
+        )
+        x = self.decoder_2(x)
+        return torch.sigmoid(x)
