c876ac725a61d19ff9bd1f2875f4fb438fde50a7
qhuy0801 <qhuy0168@gmail.com>
2023-10-17 12:46:36 +0100

    :bug: Update diffuser - training setups - reduce image size in sampling

--- src/models/trainer/diffuser.py
+++ src/models/trainer/diffuser.py
@@ -1,3 +1,4 @@
+import gc
 import os
 from datetime import datetime
 
@@ -10,7 +11,7 @@ from torchinfo import summary
 from tqdm import tqdm
 
 from models.nets.unet_v1 import UNet_v2
-from utils import linear_noise_schedule, save_checkpoint, de_normalise
+from utils import linear_noise_schedule, save_checkpoint, de_normalise, load_checkpoint
 
 
 class Diffuser:
@@ -21,13 +22,15 @@ class Diffuser:
         num_workers: int = 8,
         run_name: str = "DDPM_v1",
         output_dir: str = "./output/",
+        model_checkpoint: str = None,
         beta_start: float = 1e-4,
         beta_end: float = 0.02,
-        noise_steps: int = 50,
-        epochs: int = 5000,
-        max_lr: float = 1e-4,
+        noise_steps: int = 1000,
+        epochs: int = 10000,
+        max_lr: float = 5e-3,
         eps: float = 1e-8,
         embedding_dim: int = 256,
+        additional_note: str = "",
     ) -> None:
         super().__init__()
         # Platform
@@ -59,6 +62,14 @@ class Diffuser:
         self.model = UNet_v2(in_channels=3, out_channels=3, embedded_dim=256).to(
             self.device
         )
+
+        # If there is checkpoint for resuming training
+        if model_checkpoint is not None:
+            checkpoint = load_checkpoint(model_checkpoint, str(self.device))
+            self.model.load_state_dict(checkpoint["model"])
+
+            # Un-reference
+            checkpoint = None
 
         # Logger
         self.log = SummaryWriter(
@@ -108,10 +119,14 @@ class Diffuser:
                 f"\nEmbedding dimension: {self.embedding_dim}"
                 f"\nBatch size: {batch_size}"
                 f"\nNum workers: {num_workers}"
+                f"\nAdditional note: {additional_note}"
             )
             file.close()
 
-    def fit(self, sample_every: int = 10):
+        # Clear memory
+        gc.collect()
+
+    def fit(self, sample_every: int = 50):
         """
         Training loop trigger
         :param sample_every:
@@ -277,20 +292,20 @@ class Diffuser:
         return torch.cat([position_a, position_b], dim=-1)
 
     @torch.inference_mode()
-    def sample(self, epoch, progress_sample: {int} = {40, 30, 20, 10}):
+    def sample(self, epoch, progress_sample: {int} = {750, 500, 250}):
         """
 
         :return:
         """
         # Get a random embedding from dataset
-        _, _, semantic = next(iter(self.sample_loader))
+        image_org, _, semantic = next(iter(self.sample_loader))
         semantic = semantic.to(self.device)
 
         # Get random gaussian noise with the shape of image
-        image = torch.randn((1, 3, 128, 128)).to(self.device)
+        image = torch.randn((1, 3, 64, 64)).to(self.device)
 
         # Get an array for displaying
-        display = [de_normalise(image, self.device)]
+        display = [de_normalise(image_org, self.device), de_normalise(image, self.device)]
 
         # Put model in training mode
         self.model.eval()
@@ -340,6 +355,9 @@ class Diffuser:
             # If timestep is in the selected tuple for sampling, add to image
             if time_step in progress_sample:
                 display.append(de_normalise(image, self.device))
+
+        # Final result append
+        display.append(de_normalise(image, self.device))
 
         # Concatenation for displaying
         display = torch.cat(display, dim=0)
