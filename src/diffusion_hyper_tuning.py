9dcb8fec46f894360d1b4a2fa4ba97ddbcf32535
qhuy0801 <qhuy0168@gmail.com>
2023-11-05 23:37:31 +0000

    :fire: Hyper-parameters tuning _ 1

---
+++ src/diffusion_hyper_tuning.py
@@ -0,0 +1,49 @@
+import wandb
+
+import CONST
+from entities import WoundDataset
+from models.trainer.diffuser import Diffuser
+
+
+# Define the agent
+# Run this on agent computers
+def train():
+    """
+    Training trigger for hyper-parameter optimisation
+    :return: None
+    """
+    # Init the run
+    run = wandb.init(project="DDPM_hyper_tuning")
+
+    # Get the configuration for instance
+    config = wandb.config
+
+    # Similar approach to full train
+    # The data
+    dataset = WoundDataset(
+        image_dir=CONST.PROCESSED_IMAGES_DIR,
+        segment_dir=CONST.PROCESSED_SEGMENT_DIR,
+        target_tensor_size=CONST.DIFFUSER_SETTINGS.INPUT_SIZE,
+        embedding_dir=CONST.PROCESSED_EMBEDDING_DIR
+    )
+
+    diffuser = Diffuser(
+        dataset=dataset,
+        batch_size=8,
+        num_workers=2,
+        epochs=1000,
+        run_name=CONST.DIFFUSER_SETTINGS.RUN_NAME,
+        output_dir=CONST.DIFFUSER_SETTINGS.OUTPUT_DIR,
+        max_lr=config.learning_rate,
+        noise_steps=config.noise_steps,
+        variance_schedule_type=config.variance_schedule_type,
+        attn_heads=config.attn_heads,
+        wandb_run=run,
+    )
+
+    diffuser.fit()
+
+
+if __name__ == '__main__':
+    # Start the sweep agent
+    wandb.agent("s4cs0c7m", function=train, count=10)
