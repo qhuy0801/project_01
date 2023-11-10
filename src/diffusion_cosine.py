00b551bf4b516224f0adfe09a174075068e1ac2a
qhuy0801 <qhuy0168@gmail.com>
2023-11-10 19:44:45 +0000

    :ambulance: Update diffusion_cosine.py settings to 1000 noise steps

--- src/diffusion_cosine.py
+++ src/diffusion_cosine.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             "num_workers": 2,
             "epochs": 10000,
             "max_lr": 1e-4,
-            "noise_steps": 100,
+            "noise_steps": 1000,
         },
     )
 
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         run_name=CONST.DIFFUSER_SETTINGS.RUN_NAME,
         output_dir=CONST.DIFFUSER_SETTINGS.OUTPUT_DIR,
         max_lr=1e-4,
-        noise_steps=100,
+        noise_steps=1000,
         variance_schedule_type="cosine",
         attn_heads=1,
         wandb_run=run,
